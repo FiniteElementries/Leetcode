28efc6d855f6bd4ec7010618a8b196da7453dac1
Harry Zheng <harry.zheng@outlook.com>
2019-03-15 16:25:12 -0600

    solved p950

---
+++ p950.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def deckRevealedIncreasing(self, deck):
+        """
+        :type deck: List[int]
+        :rtype: List[int]
+        """
+        sorted_deck = deck[:]
+        sorted_deck.sort(reverse=True)
+
+        que = [sorted_deck[0]]
+        for i in range(1, len(sorted_deck)):
+            que = [sorted_deck[i]] + [que[-1]] + que[0:-1]
+
+        return que
+
+
+s = Solution()
+
+print(s.deckRevealedIncreasing([17, 13, 11, 2, 3, 5, 7]))
