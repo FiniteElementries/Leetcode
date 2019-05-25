6093d78108d27fb7b53c813bb0780217c4a5c716
Harry Zheng <harry.zheng@outlook.com>
2019-05-25 12:51:49 -0600

    simpler algorithm for p53

--- p53.py
+++ p53.py
@@ -3,36 +3,31 @@ from typing import List
 
 class Solution:
 
-    def find_max(self, nums):
-        sums1 = []
-        sums1.append(nums[0])
-        for i in range(1, len(nums)):
-            sums1.append(sums1[-1] + nums[i])
-
-        minimum = sums1[0]
-        max_dis = max(nums + sums1)
-
-        for i in range(1, len(sums1)):
-            if sums1[i] - minimum > max_dis:
-                max_dis = sums1[i] - minimum
-            if sums1[i] < minimum:
-                minimum = sums1[i]
-
-        return max_dis
-
     def maxSubArray(self, nums: List[int]) -> int:
 
-        return self.find_max(nums)
+        max_overall = nums[0]
+        max_sofar = nums[0]
+
+        for i in range(1, len(nums)):
+            tmp = nums[i] + max_sofar
+            if nums[i] > tmp:
+                max_sofar = nums[i]
+            else:
+                max_sofar = tmp
+            max_overall = max(max_sofar, max_overall)
+        return max_overall
 
 
 if __name__ == "__main__":
     s = Solution()
 
     # nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-    nums = [1]
+    # nums = [1]
     # nums = [-2, 1]
     # nums = [-1, 0, -2]
     # nums = [1, -1, -2]
     # nums = [-1, -2, -2, -2, 3, 2, -2, 0]
-    # nums = [1, 2]
+    nums = [1, 2]
+    # nums = [-2, -1]
+
     print(s.maxSubArray(nums))
