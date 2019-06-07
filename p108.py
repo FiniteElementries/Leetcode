8f0ca3cd3151e5c769884bcd3e8599274c4f2490
Harry Zheng <harry.zheng@outlook.com>
2019-06-07 14:33:37 -0600

    solved p108

---
+++ p108.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+
+        if len(nums) == 0:
+            return None
+
+        mid = len(nums) // 2
+
+        node = TreeNode(nums[mid])
+
+        node.left = self.sortedArrayToBST(nums[0:mid])
+        if mid < len(nums) - 1:
+            node.right = self.sortedArrayToBST(nums[mid + 1:])
+        return node
+
+
+if __name__ == "__main__":
+    s = Solution()
+
+    nums = [3, 5, 8]
+
+    retval = s.sortedArrayToBST(nums)
+    print(retval)
