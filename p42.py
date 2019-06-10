06c73fda9b87f4c21820a5ad9bbf6ce85845756c
Harry Zheng <harry.zheng@outlook.com>
2019-06-10 17:02:53 -0600

    solved p127

--- p42.py
+++ p42.py
@@ -1,6 +1,6 @@
+import heapq
 from typing import List
 
-import heapq
 
 class HeightHeap(object):
 
@@ -11,18 +11,19 @@ class HeightHeap(object):
     def __lt__(self, other):
         return self.height < other.height
 
+
 class Solution:
 
     def calc_volume(self, i, j):
         min_level = min(self.height[i], self.height[j])
         vol = 0
-        for m in range(i, j+1):
+        for m in range(i, j + 1):
             if min_level > self.height[m]:
                 vol += (min_level - self.height[m])
         return vol
 
     def scan_left(self, i, j):
-        for m in range(j-1, i-1, -1):
+        for m in range(j - 1, i - 1, -1):
             if self.has_water[m] == 1:
                 for n in range(m, j):
                     self.has_water[n] = 1
@@ -30,13 +31,12 @@ class Solution:
         return 0
 
     def scan_right(self, i, j):
-        for m in range(i+1, j+1):
+        for m in range(i + 1, j + 1):
             if self.has_water[m] == 1:
                 for n in range(i, m):
                     self.has_water[n] = 1
                 return self.calc_volume(i, m)
         return 0
-
 
     def trap(self, height: List[int]) -> int:
         heap = []
@@ -48,14 +48,13 @@ class Solution:
             heapq.heappush(heap, HeightHeap(height[i], i))
 
         vol = 0
-        while len(heap)>0:
+        while len(heap) > 0:
             h = heapq.heappop(heap)
             self.has_water[h.ind] = 1
             vol += self.scan_left(0, h.ind)
-            vol += self.scan_right(h.ind, len(height)-1)
+            vol += self.scan_right(h.ind, len(height) - 1)
 
         return vol
-
 
 
 if __name__ == "__main__":
