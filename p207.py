61e847b10317215c92be8df7b4c9b1964ac5309a
Harry Zheng <harry.zheng@outlook.com>
2019-07-19 14:27:51 -0600

    solved p207

---
+++ p207.py
@@ -0,0 +1,67 @@
+from typing import List
+
+
+class Solution:
+
+    def dfs(self, node, edges, pre_request_count):
+        if pre_request_count[node] < 0:
+            return
+
+        pre_request_count[node] -= 1
+        # check pre requests
+        if pre_request_count[node] > 0:
+            return
+
+        if node in edges:
+            for next_node in edges[node]:
+                self.dfs(next_node, edges, pre_request_count)
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
+            return True
+
+        edges = {}
+
+        pre_request_count = [0] * numCourses
+
+        end_nodes = set()
+
+        # create graph
+        for edge in prerequisites:
+            start = edge[1]
+            end = edge[0]
+            pre_request_count[end] += 1
+
+            end_nodes.add(end)
+
+            if start not in edges:
+                edges[start] = set()
+
+            edges[start].add(end)
+
+        for item in range(numCourses):
+            if item not in end_nodes:
+                self.dfs(item, edges, pre_request_count)
+
+        for item in pre_request_count:
+            if item > 0:
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    numCourses = 2
+    prerequisites = [[1, 0]]
+
+    numCourses = 2
+    prerequisites = [[0, 1], [1, 0]]
+
+    numCourses = 3
+    prerequisites = [[1, 0]]
+
+    # numCourses = 3
+    # prerequisites = [[1,0],[1,2],[0,1]]
+
+    print(s.canFinish(numCourses, prerequisites))
