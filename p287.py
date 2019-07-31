0ffbb29916316ad05e2fcf600be23484b603577c
Harry Zheng <harry.zheng@outlook.com>
2019-07-31 16:55:29 -0600

    inplace solution

--- p287.py
+++ p287.py
@@ -2,12 +2,17 @@ from typing import List
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        s = set()
-        for item in nums:
-            if item not in s:
-                s.add(item)
-            else:
-                return item
+
+        while True:
+            ind = nums[0]
+            tmp = nums[ind]
+
+            if ind == tmp:
+                return ind
+
+            nums[ind] = ind
+            nums[0] = tmp
+
 
 
 if __name__ == '__main__':
@@ -15,12 +20,12 @@ if __name__ == '__main__':
 
     nums = [1, 3, 4, 2, 2]
 
-    nums = [2, 2, 2, 2, 2]
-
-    nums = [1, 4, 4, 2, 4]
-
-    nums = [2, 1, 1, 1, 4]
-
-    nums = [3, 1, 3, 4, 2]
+    # nums = [2, 2, 2, 2, 2]
+    #
+    # nums = [1, 4, 4, 2, 4]
+    #
+    # nums = [2, 1, 1, 1, 4]
+    #
+    # nums = [3, 1, 3, 4, 2]
 
     print(s.findDuplicate(nums))
