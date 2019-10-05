29a247b2a88a1f897b03593d9f2567114a7cb7fa
Harry Zheng <harry.zheng@outlook.com>
2019-10-05 10:12:42 -0600

    solved p35

---
+++ p35.py
@@ -0,0 +1,17 @@
+from typing import List
+from bisect import bisect_left
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return bisect_left(nums, target)
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    nums = [1, 2, 3, 5]
+
+    target = 4
+
+    print(s.searchInsert(nums, target))
