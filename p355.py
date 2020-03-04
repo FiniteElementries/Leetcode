4c14bc81f4d395e87f8580f1f53a25e7c815d05d
Harry Zheng <harry.zheng@outlook.com>
2020-03-03 22:09:36 -0800

    better solution for p355 with heap

--- p355.py
+++ p355.py
@@ -21,6 +21,12 @@ class Twitter:
         self.tweets[userId].append((self.tweets_count, tweetId))
         self.tweets_count += 1
 
+    def _add_latest_tweet(self, followee, followee_pointer, heap):
+        if followee_pointer[followee] >= 0:
+            followee_tweets = self.tweets[followee]
+            latest_tweet = followee_tweets[followee_pointer[followee]]
+            heapq.heappush(heap, (-latest_tweet[0], followee, latest_tweet[1]))
+
     def getNewsFeed(self, userId: int) -> List[int]:
         """
         Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
@@ -28,11 +34,26 @@ class Twitter:
         followees = self.followees[userId]
         followees.add(userId)
 
-        news_feed = []
+        followee_pointer = {}
+
         for followee in followees:
-            news_feed.extend(self.tweets[followee][-10::])
-        news_feed.sort(reverse=True)
-        return [item[1] for item in news_feed[0:10]]
+            followee_pointer[followee] = len(self.tweets[followee]) - 1
+
+        heap = []
+        for followee in followees:
+            self._add_latest_tweet(followee, followee_pointer, heap)
+
+        res = []
+
+        while len(heap) > 0 and len(res) < 10:
+            latest_tweet = heapq.heappop(heap)
+            res.append(latest_tweet[2])
+
+            followee = latest_tweet[1]
+            followee_pointer[followee] -= 1
+            self._add_latest_tweet(followee, followee_pointer, heap)
+
+        return res
 
     def follow(self, followerId: int, followeeId: int) -> None:
         """
@@ -64,9 +85,18 @@ if __name__ == '__main__':
                "postTweet", "postTweet", "postTweet", "getNewsFeed"]
     datas = [[], [1, 5], [1, 3], [1, 101], [1, 13], [1, 10], [1, 2], [1, 94], [1, 505], [1, 333], [1, 22], [1, 11], [1]]
 
+    actions = ["Twitter", "postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"]
+    datas = [[], [1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+
     for action, data in zip(actions, datas):
         if action == "postTweet":
             s.postTweet(data[0], data[1])
+            print(None)
+        elif action == "follow":
+            s.follow(data[0], data[1])
+            print(None)
+        elif action == 'unfollow':
+            s.unfollow(*data)
+            print(None)
         elif action == "getNewsFeed":
             print(s.getNewsFeed(data[0]))
-
